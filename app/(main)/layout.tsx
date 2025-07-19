74acfcdd4c4815f4cbd0210dbaca7590158f9f37
Kevin Loquencio <kevin.loquencio@gmail.com>
2025-07-19 17:42:17 +0800

    add: menus

--- app/(main)/layout.tsx
+++ app/(main)/layout.tsx
@@ -6,13 +6,13 @@ interface AppLayoutProps {
 }
 
 export const metadata: Metadata = {
-    title: 'PrimeReact Sakai',
+    title: 'Sun Pleats Corporation',
     description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
     robots: { index: false, follow: false },
     viewport: { initialScale: 1, width: 'device-width' },
     openGraph: {
         type: 'website',
-        title: 'PrimeReact SAKAI-REACT',
+        title: 'PrimeReact Sun Pleats Corporation',
         url: 'https://sakai.primereact.org/',
         description: 'The ultimate collection of design-agnostic, flexible and accessible React UI Components.',
         images: ['https://www.primefaces.org/static/social/sakai-react.png'],
